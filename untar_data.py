c07d6b4b48d07bf20363dc6376c6f355890922ab
nikcheerla <nikcheerla@gmail.com>
2018-08-13 21:09:08 -0700

    sync

--- untar_data.py
+++ untar_data.py
@@ -1,23 +1,33 @@
 
 import subprocess, glob, os
 from multiprocessing import Pool
+from utils import *
+from logger import VisdomLogger
 
 tasks = ['rgb', 'normal','principal_curvature', 'depth_zbuffer']
 links = [link[:-1] for link in open('data/alllinks.txt', 'r')]
 
+logger = VisdomLogger("train", server='35.230.67.129', port=7000, env=JOB)
+
 def process_file(file):
-	*rest, task, archive = file.split('/')
-	if task not in tasks: return "skipped"
-	result_dir = "/result/" + archive[:-4]
-	os.makedirs(result_dir, exist_ok=True)
+	try:
 
-	curl = subprocess.Popen(["curl", "-s", file], stdout=subprocess.PIPE)
-	tar = subprocess.Popen(["tar", "xf", "-", "-C", result_dir, "--no-same-owner"], stdin=curl.stdout, stdout=subprocess.PIPE)
-	tar.wait()
+		*rest, task, archive = file.split('/')
+		if task not in tasks: return "skipped"
+		result_dir = "/result/" + archive[:-4]
+		os.makedirs(result_dir, exist_ok=True)
 
-	return result_dir
+		curl = subprocess.Popen(["curl", "-s", file], stdout=subprocess.PIPE)
+		tar = subprocess.Popen(["tar", "xf", "-", "-C", result_dir, "--no-same-owner"], stdin=curl.stdout, stdout=subprocess.PIPE)
+		tar.wait()
+
+		return result_dir
+	
+	except:
+		return "error"
+
 
 with Pool() as pool:
 	for i, result_dir in enumerate(pool.imap_unordered(process_file, links)):
 		if result_dir != "skipped":
-			print (f"Downloaded {result_dir}: {i}/{len(links)} files")
+			logger.text (f"Downloaded {result_dir}: {i}/{len(links)} files")
