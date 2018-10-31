40aa22a0e343b6051ea455d4f7f2adc3d8ea6dbe
Nikhil Cheerla <nikcheerla@DN0a1fed61.SUNet>
2018-10-31 12:12:04 -0700

    perceps

--- jobs/mixedperceptuals.sh
+++ jobs/mixedperceptuals.sh
@@ -1,6 +1,7 @@
 
 # job run --config "group_baseline" "python -m experiments.train_perceptuals"
-job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.05"
-job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.1"
-job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.2"
-job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.5"
+job run --config "percep_curve0.1_depth0.5" "python -m experiments.train_perceptuals --curvature-step 0.1"
+job run --config "percep_curve0.1_depth0.5" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.05"
+job run --config "percep_curve0.1depth0.1" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.1"
+job run --config "percep_curve0.1depth0.2" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.2"
+job run --config "percep_curve0.1depth0.5" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.5"
