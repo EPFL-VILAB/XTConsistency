4a589ce8dcb7f6cbc870e174f0134d34ad0783ea
Rohan Suri <rohansuri5301@gmail.com>
2018-10-30 21:25:00 -0700

    changed to groupnorm

--- modules/unet.py
+++ modules/unet.py
@@ -18,11 +18,11 @@ class UNet_up_block(nn.Module):
         super().__init__()
         self.up_sampling = nn.Upsample(scale_factor=2, mode='bilinear')
         self.conv1 = nn.Conv2d(prev_channel + input_channel, output_channel, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(output_channel)
+        self.bn1 = nn.GroupNorm(8, output_channel)
         self.conv2 = nn.Conv2d(output_channel, output_channel, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(output_channel)
+        self.bn2 = nn.GroupNorm(8, output_channel)
         self.conv3 = nn.Conv2d(output_channel, output_channel, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(output_channel)        
+        self.bn3 = nn.GroupNorm(8, output_channel)        
         self.relu = torch.nn.ReLU()
         self.up_sample = up_sample
 
@@ -40,11 +40,11 @@ class UNet_down_block(nn.Module):
     def __init__(self, input_channel, output_channel, down_size=True):
         super().__init__()
         self.conv1 = nn.Conv2d(input_channel, output_channel, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(output_channel)
+        self.bn1 = nn.GroupNorm(8, output_channel)
         self.conv2 = nn.Conv2d(output_channel, output_channel, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(output_channel)
+        self.bn2 = nn.GroupNorm(8, output_channel)
         self.conv3 = nn.Conv2d(output_channel, output_channel, 3, padding=1)
-        self.bn3 = nn.BatchNorm2d(output_channel)
+        self.bn3 = nn.GroupNorm(8, output_channel)
         self.max_pool = nn.MaxPool2d(2, 2)
         self.relu = nn.ReLU()
         self.down_size = down_size
@@ -63,20 +63,20 @@ class UNet(TrainableModel):
     def __init__(self):
         super().__init__()
 
-        self.down_block1 = UNet_down_block(3, 16, False)
-        self.down_block2 = UNet_down_block(16, 32, True)
-        self.down_block3 = UNet_down_block(32, 64, True)
-        self.down_block4 = UNet_down_block(64, 128, True)
-        self.down_block5 = UNet_down_block(128, 256, True)
-        self.down_block6 = UNet_down_block(256, 512, True)
-        self.down_block7 = UNet_down_block(512, 1024, True)
+        self.down_block1 = UNet_down_block(3, 16, False) #   256
+        self.down_block2 = UNet_down_block(16, 32, True) #   128
+        self.down_block3 = UNet_down_block(32, 64, True) #   64
+        self.down_block4 = UNet_down_block(64, 128, True) #  32
+        self.down_block5 = UNet_down_block(128, 256, True) # 16
+        self.down_block6 = UNet_down_block(256, 512, True) # 8
+        self.down_block7 = UNet_down_block(512, 1024, True)# 4 
 
         self.mid_conv1 = nn.Conv2d(1024, 1024, 3, padding=1)
-        self.bn1 = nn.BatchNorm2d(1024)
+        self.bn1 = nn.GroupNorm(8, 1024)
         self.mid_conv2 = nn.Conv2d(1024, 1024, 3, padding=1)
-        self.bn2 = nn.BatchNorm2d(1024)
+        self.bn2 = nn.GroupNorm(8, 1024)
         self.mid_conv3 = torch.nn.Conv2d(1024, 1024, 3, padding=1)
-        self.bn3 = torch.nn.BatchNorm2d(1024)
+        self.bn3 = nn.GroupNorm(8, 1024)
 
         self.up_block1 = UNet_up_block(512, 1024, 512)
         self.up_block2 = UNet_up_block(256, 512, 256)
@@ -86,7 +86,7 @@ class UNet(TrainableModel):
         self.up_block6 = UNet_up_block(16, 32, 16)
 
         self.last_conv1 = nn.Conv2d(16, 16, 3, padding=1)
-        self.last_bn = nn.BatchNorm2d(16)
+        self.last_bn = nn.GroupNorm(8, 16)
         self.last_conv2 = nn.Conv2d(16, 3, 1, padding=0)
         self.relu = nn.ReLU()
 
