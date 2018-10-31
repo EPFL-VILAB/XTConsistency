3c8fe242cfebf1e157a0018258c222d74ad6d9fc
Rohan Suri <rohansuri5301@gmail.com>
2018-10-30 21:25:57 -0700

    added CurveUNet

--- experiments/train_normal_curvature.py
+++ experiments/train_normal_curvature.py
@@ -14,7 +14,7 @@ from logger import Logger, VisdomLogger
 from datasets import ImageTaskDataset
 from torch.optim.lr_scheduler import MultiStepLR
 
-from modules.percep_nets import DenseNet, DeepNet, BaseNet, ResidualsNet, WideNet, PyramidNet
+from modules.percep_nets import DenseNet, DeepNet, BaseNet, ResidualsNet, WideNet, PyramidNet, CurveUNet
 
 import IPython
 
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
     # MODEL
     print ("Using PyramidNet")
-    model = DataParallelModel(PyramidNet())
+    model = DataParallelModel(CurveUNet())
     model.compile(torch.optim.Adam, lr=2e-4, weight_decay=2e-6, amsgrad=True)
     print (model.forward(torch.randn(1, 3, 512, 512)).shape)
 
