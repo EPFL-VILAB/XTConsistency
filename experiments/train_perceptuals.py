6297d3e01b16291f9340c4b292acf6ede3289e8a
Nikhil Cheerla <nikcheerla@DN0a1fed61.SUNet>
2018-10-31 11:49:47 -0700

    perceps

--- experiments/train_perceptuals.py
+++ experiments/train_perceptuals.py
@@ -73,7 +73,7 @@ def main(curvature_step=0, depth_step=0):
 
     # DATA LOADING
     train_loader, val_loader, test_set, test_images, ood_images, train_step, val_step = \
-        load_data("rgb", "normal", batch_size=32, batch_transforms=random_resize)
+        load_data("rgb", "normal", batch_size=48)
     logger.images(test_images, "images", resize=128)
     logger.images(torch.cat(ood_images, dim=0), "ood_images", resize=128)
     plot_images(model, logger, test_set, ood_images, mask_val=0.502,
--- jobs/mixedperceptuals.sh
+++ jobs/mixedperceptuals.sh
@@ -1,4 +1,6 @@
 
 # job run --config "group_baseline" "python -m experiments.train_perceptuals"
-job run --config "nvidia_percep_curvature" "python -m experiments.train_perceptuals --curvature-step 0.01"
-job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.01 --depth-step 0.005"
+job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.05"
+job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.1"
+job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.2"
+job run --config "nvidia_percep_curvedepth" "python -m experiments.train_perceptuals --curvature-step 0.1 --depth-step 0.5"
