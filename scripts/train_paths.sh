170b7338d607950aa2dd12676247c38d3ef20990
Rohan Suri <rohansuri5301@gmail.com>
2018-11-15 16:45:57 -0800

    scripts

--- scripts/train_paths.sh
+++ scripts/train_paths.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# [NGC DOWNLOAD job run --config "fine_tune_rn_r_x" "python -m experiments.train_path \"rn(r(x))\"";
-NGC job run --config "fine_tune_rn_Er_a_x" "python -m experiments.train_path \"rn(Er(a(x)))\"";
+# [DOWNLOAD NGC] job run --config "fine_tune_rn_r_x" "python -m experiments.train_path \"rn(r(x))\"";
 NGC job run --config "fine_tune_k3N_Ck3_RC_x" "python -m experiments.train_path \"k3N(Ck3(RC(x)))\"";
 # [DONE] job run --config "fine_tune_F_EC_a_x" "python -m experiments.train_path \"F(EC(a(x)))\"";
 # [DONE] job run --config "fine_tune_S_CE_RC_x" "python -m experiments.train_path \"S(CE(RC(x)))\"";
@@ -11,12 +10,14 @@ NGC job run --config "fine_tune_rn_Er_CE_RC_x" "python -m experiments.train_path
 [cloud2] job run --config "fine_tune_F_H_ED_a_x" "python -m experiments.train_path \"F(H(ED(a(x))))\"";
 [cloud3] job run --config "fine_tune_F_H_d_x" "python -m experiments.train_path \"F(H(d(x)))\"";
 [cloud4] job run --config "fine_tune_G_ED_a_x" "python -m experiments.train_path \"G(ED(a(x)))\"";
-NGC job run --config "fine_tune_rn_Er_a_x" "python -m experiments.train_path \"rn(Er(a(x)))\"";
-[cloud5] job run --config "fine_tune_F_RC_x" "python -m experiments.train_path \"F(RC(x))\"";
 [cloud6] job run --config "fine_tune_F_H_g_S_a_x" "python -m experiments.train_path \"F(H(g(S(a(x)))))\"";
+job run --config "fine_tune_F_RC_x" "python -m experiments.train_path \"F(RC(x))\"";
 
 [cloud1] job run --config "fine_tune_F_f_S_a_x" "python -m experiments.train_path \"F(f(S(a(x))))\""
 NGC job run --config "fine_tune_rn_nr_S_a_x" "python -m experiments.train_path \"rn(nr(S(a(x))))\"";
 NGC job run --config "fine_tune_rn_Er_a_x" "python -m experiments.train_path \"rn(Er(a(x)))\"";
 NGC job run --config "fine_tune_F_f_rn_r_x" "python -m experiments.train_path \"F(f(rn(r(x))))\"";
 
+
+NGC job run --config "fine_tune_rn_nr_F_RC_x" "python -m experiments.train_path \"rn(nr(F(RC(x))))\"";
+NGC job run --config "fine_tune_F_f_rn_r_x" "python -m experiments.train_path \"F(f(rn(r(x))))\"";
