f15febbbc1ddc8e6d2c3d3344f3e8d1db6ecd5dd
Nikhil Cheerla <nikcheerla@gmail.com>
2018-12-01 17:40:10 -0800

    functional

--- experiments/train_functional.py
+++ experiments/train_functional.py
@@ -21,9 +21,7 @@ from modules.resnet import ResNet
 from modules.unet import UNet, UNetOld
 from fire import Fire
 
-from skimage import feature
 from functools import partial
-from scipy import ndimage
 
 from transfers import TRANSFER_MAP
 
@@ -34,8 +32,7 @@ def main(loss_config="gt_mse", mode="standard", pretrained=False, **kwargs):
 
     # FUNCTIONAL LOSS
     functional = get_functional_loss(config=loss_config, mode=mode, **kwargs)
-    print(functional)
-    print ("Losses: ", functional.losses.keys())
+    print ("Functional Loss: ", functional)
 
     # MODEL
     model = TRANSFER_MAP['n'].load_model() if pretrained else DataParallelModel(UNet())
@@ -50,17 +47,16 @@ def main(loss_config="gt_mse", mode="standard", pretrained=False, **kwargs):
     functional.logger_hooks(logger)
 
     # DATA LOADING
-    ood_images = load_ood()
     train_loader, val_loader, train_step, val_step = load_train_val("rgb", "normal", batch_size=48)
     test_set, test_images = load_test("rgb", "normal")
+    ood_images = load_ood()
     logger.images(test_images, "images", resize=128)
     logger.images(torch.cat(ood_images, dim=0), "ood_images", resize=128)
 
     # TRAINING
     for epochs in range(0, 50):
-        preds_name = "start_preds" if epochs == 0 else "preds"
         plot_images(model, logger, test_set, dest_task="normal", ood_images=ood_images, 
-            loss_models=functional.plot_losses, preds_name=preds_name
+            loss_models=functional.plot_losses, preds_name=("start_preds" if epochs == 0 else "preds"),
         )
         logger.update("epoch", epochs)
 
