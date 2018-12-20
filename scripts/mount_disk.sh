74410111477a53aba57a07b657a2503bbb4ddfa2
Rohan Suri <rohansuri5301@gmail.com>
2018-12-19 19:55:04 -0500

    update mount disk to taskonomy3

--- scripts/mount_disk.sh
+++ scripts/mount_disk.sh
@@ -1,5 +1,10 @@
-sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
+# sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
 rm -rf mount/data
 sudo mkdir -p mount/data
-sudo mount -o discard,defaults,ro,noload /dev/sdb mount/data
+sudo mount -o discard,defaults,ro /dev/sdb mount/data
 sudo gcsfuse taskonomy-shared mount/shared
+
+# sudo mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdc
+# rm -rf mount/data_full
+# sudo mkdir -p mount/data_full
+# sudo mount -o discard,defaults,rw /dev/sdc mount/data_full
