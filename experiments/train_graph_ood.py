eb879c9afff9ef41100bcc4460983f103e06b0b0
Nikhil Cheerla <nikcheerla@gmail.com>
2019-01-07 02:12:10 +0530

    more ood

---
+++ experiments/train_graph_ood.py
@@ -0,0 +1,80 @@
+
+import os, sys, math, random, itertools, pickle
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from utils import *
+from plotting import *
+from models import TrainableModel, DataParallelModel
+from logger import Logger, VisdomLogger
+from task_configs import get_task, tasks, RealityTask
+from transfers import functional_transfers
+from datasets import TaskDataset
+from graph import TaskGraph
+from transfers import TRANSFER_MAP
+from fire import Fire
+import IPython
+
+
+def main():
+
+    task_list = [
+        tasks.rgb, 
+        tasks.normal, 
+        tasks.principal_curvature, 
+        tasks.sobel_edges,
+        tasks.depth_zbuffer,
+    ]
+
+    reality = RealityTask('almena', 
+        dataset=TaskDataset(buildings=['almena'], tasks=task_list),
+        tasks=task_list,
+        batch_size=4
+    )
+
+    graph = TaskGraph(
+        tasks=[reality, *task_list],
+        reality=reality,
+        edges_exclude=[
+            ('almena', 'normal'),  #remove all GT for normals
+            ('almena', 'depth_zbuffer'),  #remove all GT for depth_zbuffer
+            ('almena', 'sobel_edges'),  #remove all GT for sobel_edges
+        ],
+        anchored_tasks=[
+            reality,
+            tasks.rgb,
+            tasks.principal_curvature,
+            # tasks.depth_zbuffer,
+            # tasks.sobel_edges,
+        ],
+        reality=reality,
+        batch_size=4
+    )
+
+    print (graph.edges)
+    graph.p.compile(torch.optim.Adam, lr=4e-2)
+    graph.estimates.compile(torch.optim.Adam, lr=1e-2)
+
+    logger = VisdomLogger("train", env=JOB)
+    logger.add_hook(lambda logger, data: logger.step(), feature="energy", freq=16)
+    logger.add_hook(lambda logger, data: logger.plot(data["energy"], "free_energy"), feature="energy", freq=100)
+    logger.add_hook(lambda logger, data: graph.plot_estimates(logger), feature="epoch", freq=32)
+    logger.add_hook(lambda logger, data: graph.update_paths(logger), feature="epoch", freq=32)
+
+    graph.plot_paths(logger, dest_tasks=[tasks.depth_zbuffer], show_images=False)
+
+    for epochs in range(0, 4000):
+        logger.update("epoch", epochs)
+
+        free_energy = graph.free_energy(sample=12)
+        graph.estimates.step(free_energy)
+        logger.update("energy", free_energy)
+
+
+if __name__ == "__main__":
+    Fire(main)
+
+
