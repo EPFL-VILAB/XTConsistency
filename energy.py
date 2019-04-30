602f4caaf1e521094bf113b26e4140e40aca07d5
Nikhil Cheerla <nikcheerla@gmail.com>
2019-04-30 16:58:52 -0700

    train_energy_subset for independent graph

--- energy.py
+++ energy.py
@@ -644,6 +644,120 @@ energy_configs = {
             ),
         },
     },
+    "consistency_multiresolution_gan": {
+        "paths": {
+            "x": [tasks.rgb],
+            "~x": [tasks.rgb(size=512)],
+            "y^": [tasks.normal],
+            "~y^": [tasks.normal(size=512)],
+            "z^": [tasks.principal_curvature],
+            "n(x)": [tasks.rgb, tasks.normal],
+            "RC(x)": [tasks.rgb, tasks.principal_curvature],
+            "F(z^)": [tasks.principal_curvature, tasks.normal],
+            "F(RC(x))": [tasks.rgb, tasks.principal_curvature, tasks.normal],
+            "n(~x)": [tasks.rgb(size=512), tasks.normal(size=512)],
+            "F(RC(~x))": [tasks.rgb(size=512), tasks.principal_curvature(size=512), tasks.normal(size=512)],
+        },
+        "losses": {
+            "mse": {
+                ("train", "val"): [
+                    ("n(x)", "y^"),
+                    ("F(z^)", "y^"),
+                    ("RC(x)", "z^"),
+                    ("F(RC(x))", "y^"),
+                    ("F(RC(x))", "n(x)"),
+                ],
+                ("train_subset",): [
+                    ("F(RC(~x))", "n(~x)"),
+                ],
+            },
+            "gan": {
+                ("train_subset",): [
+                    ("n(x)", "n(~x)"),
+                ],
+            },
+        },
+        "plots": {
+            "ID": dict(
+                size=256, 
+                realities=("test", "ood"), 
+                paths=[
+                    "x",
+                    "y^",
+                    "n(x)",
+                    "F(RC(x))",
+                    "z^",
+                    "RC(x)",
+                ]
+            ),
+            "OOD": dict(
+                size=256, 
+                realities=("test", "ood"),
+                paths=[
+                    "~x",
+                    "n(~x)",
+                    "F(RC(~x))",
+                ]
+            ),
+        },
+    },
+    "consistency_multiresolution_gan_baseline": {
+        "paths": {
+            "x": [tasks.rgb],
+            "~x": [tasks.rgb(size=512)],
+            "y^": [tasks.normal],
+            "~y^": [tasks.normal(size=512)],
+            "z^": [tasks.principal_curvature],
+            "n(x)": [tasks.rgb, tasks.normal],
+            "RC(x)": [tasks.rgb, tasks.principal_curvature],
+            "F(z^)": [tasks.principal_curvature, tasks.normal],
+            "F(RC(x))": [tasks.rgb, tasks.principal_curvature, tasks.normal],
+            "n(~x)": [tasks.rgb(size=512), tasks.normal(size=512)],
+            "F(RC(~x))": [tasks.rgb(size=512), tasks.principal_curvature(size=512), tasks.normal(size=512)],
+        },
+        "losses": {
+            "mse": {
+                ("train", "val"): [
+                    ("n(x)", "y^"),
+                    ("F(z^)", "y^"),
+                    ("RC(x)", "z^"),
+                    ("F(RC(x))", "y^"),
+                    # ("F(RC(x))", "n(x)"),
+                ],
+                ("train_subset",): [
+                    ("F(RC(~x))", "n(~x)"),
+                ],
+            },
+            "gan": {
+                ("train_subset",): [
+                    ("n(x)", "n(~x)"),
+                ],
+            },
+        },
+        "plots": {
+            "ID": dict(
+                size=256, 
+                realities=("test", "ood"), 
+                paths=[
+                    "x",
+                    "y^",
+                    "n(x)",
+                    "F(RC(x))",
+                    "z^",
+                    "RC(x)",
+                ]
+            ),
+            "OOD": dict(
+                size=256, 
+                realities=("test", "ood"),
+                paths=[
+                    "~x",
+                    "n(~x)",
+                    "F(RC(~x))",
+                ]
+            ),
+        },
+    },
 }
 
 def coeff_hook(coeff):
@@ -739,6 +853,8 @@ class EnergyLoss(object):
                             loss['graphgan'+path1+path2] = 0
 
                         ## Hack: detach() first pass so only OOD is updated
+                        if path_values[path1] is None: continue
+                        if path_values[path2] is None: continue
                         logit_path1 = discriminator(path_values[path1].detach())
 
                         ## Progressively increase GAN trade-off
--- jobs/resolution.sh
+++ jobs/resolution.sh
@@ -5,6 +5,10 @@ job run --instance cloud2 --config "2FF_train_consistency_paired_resolution_gt_b
 job run --instance cloud3 --config "2FF_train_consistency_paired_resolution_cycle" "python -m experiments.train_energy consistency_paired_resolution_cycle --batch-size 24 --cont mount/shared/results_2FF_train_consistency_paired_resolution_cycle_6/graph.pth"
 job run --instance cloud4 --config "2FF_train_consistency_paired_resolution_cycle_baseline" "python -m experiments.train_energy consistency_paired_resolution_cycle_baseline --batch-size 24 --cont mount/shared/results_2FF_train_consistency_paired_resolution_cycle_baseline_5/graph.pth"
 
+
+
+job run --instance cloud6 --config "RES_consistency_multiresolution_gan" "python -m experiments.train_energy_subset consistency_multiresolution_gan --batch-size 24 --fast"
+
 # job run --config "2FF_rgb2x2normals_size512_baseline" "python -m experiments.train_energy rgb2x2normals_plots_size512 --fast --visualize" --gpus 2
 # job run --config "2FF_rgb2x2normals_size448_baseline" "python -m experiments.train_energy rgb2x2normals_plots_size448 --fast --visualize" --gpus 2
 # job run --config "2FF_rgb2x2normals_size384_baseline" "python -m experiments.train_energy rgb2x2normals_plots_size384 --fast --visualize" --gpus 2
