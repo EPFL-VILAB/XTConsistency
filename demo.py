405168dd5554a7d1cc9272549e24fa7343387744
teresa <teresa.ysq@gmail.com>
2020-02-04 23:32:28 +0000

    updated demo code to have flags for target task, input file or folder, output folder

--- demo.py
+++ demo.py
@@ -1,8 +1,17 @@
 import torch
 from torchvision import transforms
+
 from modules.unet import UNet, UNetReshade
+
 import PIL
 from PIL import Image
+
+import argparse
+import os.path
+from pathlib import Path
+import glob
+
+import pdb
 
 
 ####### TODO #######
@@ -10,42 +19,64 @@ from PIL import Image
 # 2. remove state dict key rename after renaming those in .pth files
 # 3. merge baseline and consistency output code
 
-# open image
+
+parser = argparse.ArgumentParser(description='Visualize output for a single Task')
+
+parser.add_argument('--task', dest='task', help="normal, depth or reshading")
+parser.set_defaults(task='NONE')
+
+parser.add_argument('--img_path', dest='img_path', help="path to rgb image")
+parser.set_defaults(im_name='NONE')
+
+parser.add_argument('--output_path', dest='output_path', help="path to where output image should be stored")
+parser.set_defaults(store_name='NONE')
+
+args = parser.parse_args()
+
 root_dir = '/scratch-data/shared/'
-img = Image.open(root_dir + 'ood_standard_set/abbasi-hotel-safavid-suite.png')
 trans_totensor = transforms.Compose([transforms.Resize(256, interpolation=PIL.Image.BILINEAR),
                                     transforms.CenterCrop(256),
                                     transforms.ToTensor()])
-img_tensor = trans_totensor(img)[:3].unsqueeze(0)
 trans_topil = transforms.ToPILImage()
 
-# define target task
+
+# get target task and model
 target_tasks = ['normal','depth','reshading']
+task_index = target_tasks.index(args.task)
 models = [UNet(), UNet(downsample=6, out_channels=1), UNetReshade(downsample=5)]
+target_task = args.task
+model = models[task_index]
 
 
-for target_task, model in zip(target_tasks,models):
+def save_outputs(img_path, output_file_name):
+
+    img = Image.open(img_path)
+    img_tensor = trans_totensor(img)[:3].unsqueeze(0)
 
     # compute baseline output
-    path = root_dir + 'results_CH_baseline_unet_'+target_task+'target_nosqerror_dataaug_contlr3e-5_1/model.pth'
+    path = root_dir + 'results_CH_baseline_unet_'+args.task+'target_nosqerror_dataaug_contlr3e-5_1/model.pth'
     model_state_dict = torch.load(path)
     model_state_dict = {k[22:]: v for k, v in model_state_dict.items()}
     model.load_state_dict(model_state_dict)
     baseline_output = model(img_tensor)
+    trans_topil(baseline_output[0]).save(args.output_path+'/'+output_file_name+'_'+args.task+'_baseline'+'.jpg')
 
     # compute consistency output
-    path = root_dir + 'results_CH_lbp_all_'+target_task+'target_gradnorm_unnormalizedmse_imagenet_nosqerror_nosqinitauglr_dataaug_1/graph.pth'
-    if target_task == 'depth': target_task = 'depth_zbuffer'
-    model_state_dict = torch.load(path)["('rgb', '"+target_task+"')"]
+    path = root_dir + 'results_CH_lbp_all_'+args.task+'target_gradnorm_unnormalizedmse_imagenet_nosqerror_nosqinitauglr_dataaug_1/graph.pth'
+    alt_name = args.task+'_zbuffer' if args.task == 'depth' else args.task
+    model_state_dict = torch.load(path)["('rgb', '"+alt_name+"')"]
     model_state_dict = {k[28:]: v for k, v in model_state_dict.items()}
     model.load_state_dict(model_state_dict)
     consistency_output = model(img_tensor)
+    trans_topil(consistency_output[0]).save(args.output_path+'/'+output_file_name+'_'+args.task+'_consistency'+'.jpg')
 
-    # save image
-    out_stacked = Image.new('RGB', (256*3, 256))
-    x_offset = 0
-    for im in [img_tensor,baseline_output,consistency_output]:
-      out_stacked.paste(trans_topil(im[0]), (x_offset,0))
-      x_offset += im.size(2)
 
-    out_stacked.save(target_task+'_out.jpg')
+img_path = Path(args.img_path)
+if img_path.is_file():
+    save_outputs(args.img_path, os.path.splitext(os.path.basename(args.img_path))[0])
+elif img_path.is_dir():
+    for f in glob.glob(args.img_path+'/*'):
+        save_outputs(f, os.path.splitext(os.path.basename(f))[0])
+else:
+    print("invalid file path!")
+    sys.exit()
